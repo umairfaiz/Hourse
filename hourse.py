493a8f59db27014d6c165d7bbba957a20671e9ff
Travis Hyyppa <travishyyppa@gmail.com>
2013-07-19 19:57:45 -0600

    Initial commit

---
+++ hourse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+
+# Import libraries
+import os
+import sys
+import subprocess
+
+class Hourse:
+	def __init__(self):
+		print "init"
+
+
+if __name__ == "__main__":
+	g = Hourse()
